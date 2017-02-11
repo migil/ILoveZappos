7658f99b5a976c2b62b6fdafed3f6d32f5bc7075
migil <migil@ucsd.edu>
2017-02-10 23:38:45 -0800

    final_incomplete

---
+++ app/src/main/java/justingil/ilovezappos/Cart.java
@@ -0,0 +1,14 @@
+package justingil.ilovezappos;
+
+import android.os.Bundle;
+
+/**
+ * Created by justingil1748 on 2/10/17.
+ */
+
+public class Cart {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cartlist);
+    }
+}
